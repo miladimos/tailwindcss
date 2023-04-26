818c5325b5af52ed185dce9deea896012efba20f
miladimos <milad.jafari6210@gmail.com>
2023-04-27 01:43:42 +0330

    edit discord

--- discord/tailwind.config.js
+++ discord/tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem"
+      }
+    },
+    extends: {
+      colors: {
+        primary: "#404eed",
+        black: "#23272a",
+      }
     },
     extend: {},
   },
